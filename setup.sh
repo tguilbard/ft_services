52499aa3226b9bbfeb6dca4e12b4f95d719d298b
tguilbard <tanguy.guilbard@free.fr>
2020-09-22 11:06:53 +0200

    building and apply in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-services = {"FTPS", "Grafana", "Nginx", "PhpMyAdmin", "WordPress", "InfluxDB", "MySQL"};
+services="nginx ftps wordpress phpmyadmin mysql influxdb grafana"
+
+ft_set_service()
+{
+	for service in $services
+	do
+		echo "Building $service"
+		docker build -t image-$service srcs/$service && kubectl apply -f srcs/$service/deployement-$service.yaml
+	done
+}
+
+ft_set_service
