25aa60b558c373a35b16441b659475c372aafb87
tguilbard <tanguy.guilbard@free.fr>
2020-09-28 14:38:05 +0200

    patch ssh

--- setup.sh
+++ setup.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
 services="nginx" # ftps wordpress phpmyadmin mysql influxdb grafana"
-
+GREEN="\e[32m\e[1m"
+DEFAULT="\e[0m"
 ft_set_service()
 {
 	for service in $services
 	do
-		echo "Building $service"
-		docker build -t image-$service srcs/$service
-	    kubectl apply -f srcs/$service/deployement-$service.yaml
+		echo -e "${GREEN}Building $service${DEFAULT}"
+		docker build -t image-$service srcs/$service | grep Step
+	    kubectl apply -f srcs/$service/deployement-$service.yaml > /dev/null
+		echo -e "${GREEN}$service is set${DEFAULT}"
 	done
 }
 
 # ================================================================
 
-minikube start --driver=docker --cpus=4 --memory=4400 --extra-config=apiserver.service-node-port-range=1-35000
+echo -en "\e[35m"; minikube start --driver=docker --cpus=4 --memory=4400 --extra-config=apiserver.service-node-port-range=1-35000
 if [[ $? == 0 ]]
 then
 	eval $(minikube docker-env)
@@ -25,12 +27,13 @@ fi
 
 # ======================================================
 
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
-kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
-kubectl apply -f srcs/load_balancer/deployement-metallb.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml > /dev/null
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml > /dev/null
+kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)" > /dev/null
+kubectl apply -f srcs/load_balancer/deployement-metallb.yaml > /dev/null
+echo -e "${GREEN}Mettallb installed${DEFAULT}"
 
 # ======================================================
 ft_set_service
 
-minikube dashboard
+echo -en "\e[35m"; minikube dashboard
--- srcs/nginx/setup.sh
+++ srcs/nginx/setup.sh
@@ -3,7 +3,8 @@
 openrc
 touch /run/openrc/softlevel
 service nginx start
-echo -e "$SSH_PASSWORD\n$SSH_PASSWORD" | adduser $SSH_USER
+adduser -D user42
+echo "user42:user42" | chpasswd
 rc-update add sshd
 rc-status
 /etc/init.d/sshd start
