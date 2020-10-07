40a06e5d7df86108f29c0a68a4196ded8b0a1745
user42 <tguilbar@student.42.fr>
2020-10-07 09:44:22 +0200

    patch wordpress

--- srcs/wordpress/wp-config.php
+++ srcs/wordpress/wp-config.php
@@ -23,7 +23,7 @@
 define( 'DB_NAME', 'wordpress' );
 
 /** MySQL database username */
-	define( 'DB_USER', 'root' );
+	define( 'DB_USER', 'admin' );
 
 /** MySQL database password */
 	define( 'DB_PASSWORD', 'password' );
