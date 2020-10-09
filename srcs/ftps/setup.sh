55dd52c75dfd19bee5a67a0320f914793f6eae58
user42 <tguilbar@student.42.fr>
2020-10-09 16:31:33 +0200

    ftp to ftps

--- srcs/ftps/setup.sh
+++ srcs/ftps/setup.sh
@@ -1,2 +1,12 @@
+yes "" | openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+-keyout /etc/ssl/private/pure-ftpd.pem -out /etc/ssl/private/pure-ftpd.pem 
+
+adduser -D "admin"
+echo "admin:password" | chpasswd
+
+openrc
+touch /run/openrc/softlevel
+rc-update add telegraf
+
 telegraf &
-pure-ftpd -p 21000:21000 -P 172.17.0.3
+pure-ftpd -j -Y 2 -p 21000:21000 -P 172.17.0.2
